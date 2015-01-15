fc7b946b659111a0bafd30e05c4ab9ac995b7458
Philip Hughes <philip@h4w5.com>
2015-01-15 17:27:03 -0500

    linked in oauth up and working v1

--- config.ru
+++ config.ru
@@ -1,8 +1,10 @@
+require 'pry'
 require 'redis'
+require 'httparty'
 require 'sinatra/base'
 require 'sinatra/reloader'
 
 
 require_relative 'server'
 
-run Bc_boards::Server 
+run Bc_boards::Server
--- server.rb
+++ server.rb
@@ -1,29 +1,72 @@
-module Bc_boards 
-	class Server < Sinatra::Base 
+module Bc_boards
+	class Server < Sinatra::Base
 
-		configure :development do 
+		enable :sessions
+
+		configure :development do
 			register Sinatra::Reloader
-			$redis = Redis.new 
-		end 
+			$redis = Redis.new
+		end
 
-		get('/') do 
-	      query_params = URI.encode_www_form({
-	        :response_type => "code",
-	        :client_id     => ENV["Linkedin_Oauth_API_Key"],
-	        :state         => "D8DCWLC327HVM",
-	        :redirect_uri  => "http://localhost:9292/linkedin/oauth_callback"
-	      })
-	      @linkedin_auth_url = "https://www.linkedin.com/uas/oauth2/authorization?" + query_params
+		get('/') do
+			if session[:access_token]
+				redirect to("/news")
+			end
+
+      query_params = URI.encode_www_form({
+        :response_type => "code",
+        :client_id     => ENV["Linkedin_Oauth_API_Key"],
+        :state         => "D8DCWLC327HVM",
+        :redirect_uri  => "http://localhost:9292/linkedin/oauth_callback"
+      })
+      @linkedin_auth_url = "https://www.linkedin.com/uas/oauth2/authorization?" + query_params
 		  render :erb, :index, :layout => :default
 		end
 
-		get('/new') do 
+		get '/linkedin/oauth_callback' do
+			# make a request to get an access token
+			response = HTTParty.post(
+        "https://www.linkedin.com/uas/oauth2/accessToken",
+        :body => {
+          :grant_type     => "authorization_code",
+          :code           => params[:code],
+          :redirect_uri   => "http://localhost:9292/linkedin/oauth_callback",
+          :client_id      => ENV["Linkedin_Oauth_API_Key"],
+          :client_secret  => ENV["Linkedin_Oauth_Secret_Key"]
+        },
+        :headers => {
+          "Accept"        => "application/json"
+        }
+      )
+
+			# save the access token for whenever we need it!
+      session[:access_token] = response["access_token"]
+
+      # use the access token to ask for name and email as json
+      response = HTTParty.get(
+      	"https://api.linkedin.com/v1/people/~:(first-name,last-name,email-address)?format=json",
+        :headers => {
+          "Authorization" => "Bearer #{session[:access_token]}"
+        }
+      )
+
+      session["name"] = response["firstName"] + " " + response["lastName"]
+      # binding.pry
+      redirect to("/news")
+	  end
+
+		get('/new') do
 			render :erb, :new
-		end	
+		end
 
 		get('/signup') do
 			render :erb, :sign_up
-		end 
+		end
+
+		get("/news") do
+			@name = session["name"]
+			render :erb, :news, :layout => :default
+		end
 
 	end
 end
